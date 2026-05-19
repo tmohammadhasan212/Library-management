dd20737b667b00854277e024e9ea286faa229ca7
Mohammad Hasan Talebi <tmohammadhasan.2001@gmail.com>
2026-05-19 16:14:16 +0330

    add a setup method and test a single book creation

--- tests/test_refactored_book.py
+++ tests/test_refactored_book.py
@@ -1,4 +1,16 @@
 import pytest
+from library_system.book_refactored import Book, BookGenerator
 
 class TestBook:
-    
+    def setup_method(self):
+        BookGenerator.reset_counter()
+
+    def test_single_book_creation(self):
+        book = Book(title="Harry Potter", author="J.K. Rowling")
+        generator = BookGenerator(book)
+        generated_book = generator.get_book()
+
+        assert generated_book.uid == 1
+        assert generated_book.title == "harry potter"
+        assert generated_book.author == "j.k. rowling"
+        assert generated_book.status == "available"
