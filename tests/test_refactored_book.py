1551ddb9a1bcc4bc5a7bd1f252261f35b86b12a7
Mohammad Hasan Talebi <tmohammadhasan.2001@gmail.com>
2026-05-19 16:16:54 +0330

    Test book with existing uid and test generate many books

--- tests/test_refactored_book.py
+++ tests/test_refactored_book.py
@@ -14,3 +14,25 @@ class TestBook:
         assert generated_book.title == "harry potter"
         assert generated_book.author == "j.k. rowling"
         assert generated_book.status == "available"
+    
+    def test_book_with_existing_uid(self):
+        book = Book(title="1984", author="George Orwell", uid=5)
+        generator = BookGenerator(book)
+        assert generator.get_book().uid == 5
+
+        new_book = Book(title="Dune", author="Frank Herbert")
+        new_gen = BookGenerator(new_book)
+        assert new_gen.get_book().uid == 6  # Counter incremented correctly
+
+    def test_generate_many_books(self):
+        books = [
+            Book(title="Book One", author="Author A"),
+            Book(title="Book Two", author="Author B"),
+            Book(title="Book Three", author="Author C")
+        ]
+        generated_books = BookGenerator.generate_many(books)
+
+        assert [b.uid for b in generated_books] == [1, 2, 3]
+        # Check lowercasing
+        assert generated_books[0].title == "book one"
+        assert generated_books[1].author == "author b"
