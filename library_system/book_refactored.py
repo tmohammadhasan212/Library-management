e75bf8da2e16dde852ec9d8755cd957a2b199714
Mohammad Hasan Talebi <tmohammadhasan.2001@gmail.com>
2026-05-19 15:32:14 +0330

    Add BookGenerator with UID assignment and counter management

--- library_system/book_refactored.py
+++ library_system/book_refactored.py
@@ -1,8 +1,25 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, model_validator, PositiveInt
 from typing import Annotated, Literal
 
 class Book(BaseModel):
     title : Annotated[str, Field(min_length=3)]
-    uid : int | None = None
+    uid : PositiveInt | None = None
     author : Annotated[str, Field(min_length=3)]
     status : Literal['available', 'borrowed'] = 'available'
+
+    
+class BookGenerator:
+    __counter = 0
+    def __init__(self, book: Book) -> None:
+        self.book = self._validate_and_set_uid(book)
+
+    def _validate_and_set_uid(self, book : Book) -> Book:
+        if book.uid is None:
+            BookGenerator.__counter += 1
+            book.uid = BookGenerator.__counter
+        else:
+            if book.uid > BookGenerator.__counter:
+                BookGenerator.__counter = book.uid
+        return book
+    
+
