2340a11b77ae96b832f8ac1bd54671ff7f156688
Mohammad Hasan Talebi <tmohammadhasan.2001@gmail.com>
2026-05-19 18:27:15 +0330

    Introduce BorrowRecord model in models/borrow_record.py
    - Uses ConfigDict for automatic string lowercasing, trimming, and length constraints
    - Includes UID, borrower_name, author, status, borrow_time, and return_time fields
    - Ensures assignment validation and enforces domain constraints

--- models/borrow_record.py
+++ models/borrow_record.py
@@ -1,2 +1,20 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, PositiveInt, ConfigDict
+from typing import Literal
+from uuid import UUID, uuid4
+
+class BorrowRecord(BaseModel):
+    model_config = ConfigDict(
+        validate_assignment=True,
+        str_to_lower=True,
+        str_strip_whitespace=True,
+        str_min_length=3,
+        str_max_length= 30
+    )
+    uid: UUID = Field(default_factory=uuid4)
+    borrower_name: str
+    author: str
+    status: Literal['borrowed', 'available'] = 'borrowed'
+    borrow_time: float
+    return_time: float | None = None
+
 
